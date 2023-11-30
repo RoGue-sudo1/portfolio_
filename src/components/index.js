845cd68d45f35c469b112524947ed2543565e64b
Harsh Rathore <harshrathore7514@gmail.com>
2023-11-30 17:03:45 +0530

    some chnages

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,10 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas,  StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-
 import Contact from './Contact';
 
 export {
@@ -18,6 +17,5 @@ export {
   Contact,
   EarthCanvas, 
   BallCanvas, 
-  ComputersCanvas, 
   StarsCanvas
 }
